89e3d5bb8f9e85c9bd7398716b6bf283dcf84367
jacob-ferrell <michaeljacobferrell@gmail.com>
2022-11-19 11:21:08 -0500

    configured MyTickets to use react-query for state

--- src/components/MyTickets.js
+++ src/components/MyTickets.js
@@ -1,60 +1,41 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import TicketsTable from './tables/TicketsTable';
-import { Spinner } from 'react-bootstrap';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import TicketsTable from "./tables/TicketsTable";
+import { Spinner } from "react-bootstrap";
+import { fetchProjects } from "../api";
+import { useQuery } from "react-query";
 
-const MyTickets = props => {
+const MyTickets = (props) => {
+  const userData = props.userData;
+  const projects = useQuery("projects", fetchProjects);
 
-    const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
-
-    const userData = props.userData || getLocal('userData');
-    const projectData = props.projectData || getLocal('projectData');
-    const getLocal = props.getLocal;
-    const fetchData = props.fetchData;
-
-    useEffect(() => {
-        setLoading(true);
-        if (userData && projectData) return setLoading(false);
-        fetch('/isUserAuth', {
-            headers: {
-                'x-access-token': localStorage.getItem('token')
-            }
-        })
-        .then(res => res.json())
-        .then(data => !data.isLoggedIn ? navigate('/login') : fetchData())
-        .finally(() => setLoading(false))
-
-    }, [])
-
-    return (
-        <div className='p-3 w-auto'>
-            <div className='my-tickets bg-light shadow rounded p-2'>
-                <div className='my-tickets-header d-flex justify-content-between'>
-                    <h5>
-                        {loading &&
-                        <Spinner 
-                        animation='border'
-                        as='span'
-                        size='sm'
-                        role='status'
-                        aria-hidden='true' 
-                        />
-                        }
-                        {loading ? ' Loading Tickets...' : 'Tickets'}
-                    </h5>
-                </div>
-                {!loading && (
-                    <TicketsTable
-                        sortBy='creator' 
-                        projectData={props.projectData}
-                        userData={props.userData}
-                        getLocal={props.getLocal}
-                    /> 
-                )}
-            </div>
+  return (
+    <div className="p-3 w-auto">
+      <div className="my-tickets bg-light shadow rounded p-2">
+        <div className="my-tickets-header d-flex justify-content-between">
+          <h5>
+            {projects.isLoading && (
+              <Spinner
+                animation="border"
+                as="span"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+            )}
+            {projects.isLoading ? " Loading Tickets..." : "My Tickets"}
+          </h5>
         </div>
-    );
-}
+        {!projects.isLoading && (
+          <TicketsTable
+            sortBy="creator"
+            projectData={projects.data}
+            userData={props.userData}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default MyTickets;
