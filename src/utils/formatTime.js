dbd827df449129951ea83c4a9fd39e949ed0d217
jacob-ferrell <michaeljacobferrell@gmail.com>
2022-12-01 11:18:01 -0500

    fixed formatTime util function.  It was rendering 12pm as 0

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,11 +1,15 @@
 export function formatTime(date) {
   date = new Date(date);
-  let end = 'AM';
+  let end = "AM";
   const hours = date.getHours();
-  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, "0");
   const seconds = date.getSeconds();
 
-  if (hours > 12) end = 'PM';
+  if (hours >= 12) {
+    end = "PM";
+  }
 
-  return (hours % 12) + ':' + minutes + end;
+  if (hours > 12) hours %= 12;
+
+  return hours + ":" + minutes + end;
 }
