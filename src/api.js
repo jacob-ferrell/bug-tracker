4bade00481cde4bc4ef4debb03271bc6bbda8afd
jacob-ferrell <michaeljacobferrell@gmail.com>
2022-12-11 20:09:01 -0500

    updated fetch urls

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 async function fetchURL(url, data = null) {
+  const server = 'https://bug-tracker-backend-934x.onrender.com';
   const req = {
     headers: {
       "Content-type": "application/json",
@@ -8,26 +9,26 @@ async function fetchURL(url, data = null) {
     req.headers["x-access-token"] = localStorage.getItem("token");
   req.method = data ? "POST" : "GET";
   if (data) req.body = JSON.stringify(data);
-  const res = await fetch(url, { ...req });
+  const res = await fetch((server + url), { ...req });
   const json = await res.json();
   if (json.failed) alert(json.message);
   if (json.isLoggedIn == false) logout();
   return json;
 }
 const fetchProjects = async () => {
-  return await fetchURL("/getProjectData");
+  return await fetchURL((server + "/getProjectData"));
 };
 
 const fetchTeam = async () => {
-  return await fetchURL("/getTeamMembers");
+  return await fetchURL((server + "/getTeamMembers"));
 };
 
 const fetchUser = async () => {
-  return await fetchURL("/isUserAuth");
+  return await fetchURL((server + "/isUserAuth"));
 };
 
 const fetchNotifications = async () => {
-  return await fetchURL("/getNotifications");
+  return await fetchURL((server + "/getNotifications"));
 };
 
 function logout() {
