3222308de32f48caf67599e6fa2f430e34137a28
jacob-ferrell <michaeljacobferrell@gmail.com>
2022-12-11 22:49:46 -0500

    added .htaccess file attempting to resolve routing issue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import { React } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider, QueryClient } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 
